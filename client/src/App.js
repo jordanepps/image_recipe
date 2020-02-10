ceffcb46499eb0b79f526f6b993fa6c00641de25
Jordan Epps <jordanepps@gmail.com>
2020-02-10 11:39:43 -0500

    Remove unsed code and add axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
 import React, { useState } from 'react';
 import './App.css';
 
+import axios from 'axios';
+
 //
 import ImageUploader from './components/ImageUploader';
 
 function App() {
   const [files, setFiles] = useState([]);
+
+  const handleUpload = () => {
+    console.log('test');
+  };
+
   return (
     <div className="App">
       <h1>Image Recipe</h1>
       <ImageUploader files={files} setFiles={setFiles} />
+      <button onClick={handleUpload} disabled={!files.length}>
+        Upload
+      </button>
     </div>
   );
 }
